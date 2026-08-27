821dd88255cc185203d01c4e097b573dd8c336b5
Dew25 <juri.melnikov@ivkhk.ee>
2026-08-27 12:56:48 +0300

    уменьшили размер иконок таб. тех карт

--- src/main/resources/static/js/models.js
+++ src/main/resources/static/js/models.js
@@ -514,13 +514,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 <td>${formatPrice(techmap.price)}</td>
                 <td>
                     <div class="flex gap-2">
-                        <button class="btn btn-sm btn-info" onclick="editTechmap(${techmap.id})">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <button class="btn btn-xs btn-info h-6 min-h-6 px-2" onclick="editTechmap(${techmap.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                             </svg>
                         </button>
-                        <button class="btn btn-sm btn-error" onclick="deleteTechmap(${techmap.id})">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <button class="btn btn-xs btn-error h-6 min-h-6 px-2" onclick="deleteTechmap(${techmap.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
                         </button>
