3d1673c5f20a790d27fcc218aa287314063d6c71
Dew25 <juri.melnikov@ivkhk.ee>
2026-08-30 01:54:58 +0300

    уменьшаем строки таб. выполненной работы

--- src/main/resources/static/js/work-results.js
+++ src/main/resources/static/js/work-results.js
@@ -881,8 +881,8 @@ function renderWorkResultsTable(workResults, orders) {
             <td>${completedQuantity} / ${remainingQuantity}</td>
             <td>${formatEuros(totalCost)}</td>
             <td>
-                <button class="btn btn-sm btn-error" onclick="deleteWorkResult(${workResult.id})">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <button class="btn btn-sm btn-error" onclick="deleteWorkResult(${workResult.id})" style="height: 25px; min-height: 25px; padding: 2px 6px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="width: 25px; height: 25px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                 </button>
