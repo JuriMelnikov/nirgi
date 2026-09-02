2a3d3a023da166f2c42783b47e182036c4407300
Dew25 <juri.melnikov@ivkhk.ee>
2026-09-02 20:49:33 +0300

    Увеличил шрифт в таблице "Существующие технологические карты" (5)

--- src/main/resources/static/js/models.js
+++ src/main/resources/static/js/models.js
@@ -530,11 +530,11 @@ document.addEventListener('DOMContentLoaded', function() {
             row.classList.add('hover:bg-warning/20');
             row.onclick = () => editTechmap(techmap.id);
             row.innerHTML = `
-                <td class="text-xs text-center" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 40px;">${techmap.serial}</td>
-                <td class="text-xs text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb;">${techmap.descriptor}</td>
-                <td class="text-xs text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 80px;">${techmap.time}</td>
-                <td class="text-xs text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 80px;">${formatPrice(techmap.price)}</td>
-                <td class="text-xs text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 80px;">
+                <td class="text-sm text-center" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 40px;">${techmap.serial}</td>
+                <td class="text-sm text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb;">${techmap.descriptor}</td>
+                <td class="text-sm text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 80px;">${techmap.time}</td>
+                <td class="text-sm text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 80px;">${formatPrice(techmap.price)}</td>
+                <td class="text-sm text-left" style="padding: 0px 4px; height: 25px; border: 1px solid #e5e7eb; width: 80px;">
                     <button class="btn btn-xs btn-error h-4 min-h-4 px-1" onclick="event.stopPropagation(); deleteTechmap(${techmap.id})">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
